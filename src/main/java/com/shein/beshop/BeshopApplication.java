6f110f2142480fd27010fe5391dd40fa53c04d51
eralash911 <unreal725@gmail.com>
2022-11-29 01:57:36 +0300

    added some forms

--- src/main/java/com/shein/beshop/BeshopApplication.java
+++ src/main/java/com/shein/beshop/BeshopApplication.java
@@ -2,12 +2,19 @@ package com.shein.beshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.security.auth.login.Configuration;
 
 @SpringBootApplication
 public class BeshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeshopApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(BeshopApplication.class, args);
+		PasswordEncoder encoder = context.getBean(PasswordEncoder.class);
+		System.out.println(encoder.encode("pass"));
 	}
 
 }
